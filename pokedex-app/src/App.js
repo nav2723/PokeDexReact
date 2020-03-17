5a823931b40de39f406202f1d70cafc0c47bf950
nav2723 <nas162@pitt.edu>
2020-03-16 21:41:03 -0400

    Initializing app and Navbar

---
+++ pokedex-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from './components/layout/NavBar'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
